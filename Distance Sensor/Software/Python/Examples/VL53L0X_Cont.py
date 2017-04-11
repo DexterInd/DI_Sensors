fd33f904d87ea1bdea2c3c4db8b5e745a2b9b0d7
Matthew Richardson <matt@dexterindustries.com>
2017-04-11 11:57:38 -0400

    Add Distance Sensor Python drivers

---
+++ Distance Sensor/Software/Python/Examples/VL53L0X_Cont.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# https://www.dexterindustries.com/
+#
+# Copyright (c) 2017 Dexter Industries
+# Released under the MIT license (http://choosealicense.com/licenses/mit/).
+# For more information see https://github.com/DexterInd/BrickPi3/blob/master/LICENSE.md
+#
+# Python example program for the VL53L0X Distance Sensor
+
+import time
+import VL53L0X
+
+sensor = VL53L0X.VL53L0X()
+
+sensor.init()
+sensor.setTimeout(500)
+
+# Start continuous back-to-back mode (take readings as
+# fast as possible).  To use continuous timed mode
+# instead, provide a desired inter-measurement period in
+# ms (e.g. sensor.startContinuous(100)).
+sensor.startContinuous()
+while True:
+    print("%4dmm" % sensor.readRangeContinuousMillimeters())
+    if sensor.timeoutOccurred():
+        print(" TIMEOUT")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DI_Sensors
 Dexter Industries Sensors 
+
+License
+-------
+
+Please review the [LICENSE.md] file for license information.
+
+[LICENSE.md]: ./LICENSE.md
