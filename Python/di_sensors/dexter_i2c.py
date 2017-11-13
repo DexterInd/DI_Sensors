3e67bd37d9d1933cea1181f811c608ff1a9f812a
Nicole Parrot <cleoqc1124@gmail.com>
2017-11-13 08:33:19 -0500

    add couple of comments

--- Python/di_sensors/dexter_i2c.py
+++ Python/di_sensors/dexter_i2c.py
@@ -11,6 +11,10 @@ from __future__ import division
 
 import time
 
+
+# Enable one of the communication libraries
+# This is not meant to change on a regular basis
+# If Periphery doesn't work for you, uncomment either pigpio or smbus
 #RPI_1_Module = "pigpio"
 #RPI_1_Module = "smbus"
 RPI_1_Module = "periphery"
