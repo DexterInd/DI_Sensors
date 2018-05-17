5523c2a2b4672c4f8b997e52c072acec77fc1fb7
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2018-05-17 21:10:34 +0300

    fix - install sources (#50)

--- Install/update_sensors.sh
+++ Install/update_sensors.sh
@@ -165,7 +165,7 @@ install_scriptools_and_rfrtools() {
   fi
 
   # update script_tools first
-  curl --silent -kL https://raw.githubusercontent.com/RobertLucian/script_tools/feature/strip-down-scriptools/install_script_tools.sh > $PIHOME/.tmp_script_tools.sh
+  curl --silent -kL https://raw.githubusercontent.com/DexterInd/script_tools/develop/install_script_tools.sh > $PIHOME/.tmp_script_tools.sh
   echo "Installing script_tools. This might take a while.."
   bash $PIHOME/.tmp_script_tools.sh $selectedbranch > /dev/null
   ret_val=$?
@@ -189,7 +189,7 @@ clone_disensors() {
   # it's simpler and more reliable (for now) to just delete the repo and clone a new one
   # otherwise, we'd have to deal with all the intricacies of git
   sudo rm -rf $SENSOR_DIR
-  git clone --quiet --depth=1 -b feature/use-rfr-tools-too https://github.com/RobertLucian/DI_Sensors.git
+  git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/DI_Sensors.git
   cd $SENSOR_DIR
 }
 
