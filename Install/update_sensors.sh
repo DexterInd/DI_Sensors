d434748644c65a94f5db853771342015dc4b6e95
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-08-17 19:46:47 +0300

    hotfix - change references temporarily

--- Install/update_sensors.sh
+++ Install/update_sensors.sh
@@ -161,7 +161,8 @@ install_rfrtools_repo() {
 
   # if rfrtools is not bypassed then install it
   if [[ $install_rfrtools = "true" ]]; then
-    curl --silent -kL https://raw.githubusercontent.com/DexterInd/RFR_Tools/$selectedbranch/scripts/install_tools.sh > $PIHOME/.tmp_rfrtools.sh
+    # curl --silent -kL https://raw.githubusercontent.com/DexterInd/RFR_Tools/$selectedbranch/scripts/install_tools.sh > $PIHOME/.tmp_rfrtools.sh
+    curl --silent -kL https://raw.githubusercontent.com/RobertLucian/DI_Sensors/hotfix/add-gui-installation/Install/update_sensors.sh > $PIHOME/.tmp_rfrtools.sh
     echo "Installing RFR_Tools. This might take a while.."
     bash $PIHOME/.tmp_rfrtools.sh ${rfrtools_options[@]} # > /dev/null
     ret_val=$?
@@ -189,7 +190,8 @@ clone_disensors() {
   # it's simpler and more reliable (for now) to just delete the repo and clone a new one
   # otherwise, we'd have to deal with all the intricacies of git
   sudo rm -rf $SENSOR_DIR
-  git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/DI_Sensors.git
+  # git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/DI_Sensors.git
+  git clone --quiet --depth=1 -b hotfix/add-gui-installation https://github.com/RobertLucian/DI_Sensors.git
   cd $SENSOR_DIR
 }
 
